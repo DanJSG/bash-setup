9105a127778c41a8a3d4ad92ec0fcc4f47c3e09f
dan <jackson.daniel365@gmail.com>
2020-06-12 01:32:00 +0100

    Added basic setup logging

--- setup.sh
+++ setup.sh
@@ -1,50 +1,56 @@
 #!/bin/bash
 
+LOGDIR=~/Documents/usersetup/
+LOGPATH="${LOGDIR}setup.log"
+
+sudo mkdir $LOGDIR
+sudo touch $LOGPATH
+
 # Add MongoDB key
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee -a /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 # Ensure apt package manager up to date
-sudo apt-get update
-sudo apt upgrade
+sudo apt-get update -y | sudo tee -a $LOGPATH
+sudo apt upgrade -y | sudo tee -a $LOGPATH
 
 # Useful tools
-sudo apt-get install -y wget
-sudo apt install -y curl
-sudo apt-get install -y net-tools
-sudo apt-get install -y gparted
+sudo apt-get install -y wget | sudo tee -a $LOGPATH
+sudo apt install -y curl | sudo tee -a $LOGPATH
+sudo apt-get install -y net-tools | sudo tee -a $LOGPATH
+sudo apt-get install -y gparted | sudo tee -a $LOGPATH
 
 # Essential software
-sudo apt-get install -y git
-sudo apt-get install -y python3
-sudo apt-get install -y pip3
-sudo apt install default-jdk
-sudo apt-get install -y gcc
-sudo apt-get install -y g++
-sudo apt install -y mysql-server
-sudo apt-get install -y mongodb-org
-sudo apt-get install -y vlc
-sudo apt-get install -y gimp
-sudo apt-get install -y inkscape
-sudo apt install -y chromium-browser
+sudo apt-get install -y git | sudo tee -a $LOGPATH
+sudo apt-get install -y python3 | sudo tee -a $LOGPATH
+sudo apt-get install -y pip3 | sudo tee -a $LOGPATH
+sudo apt install default-jdk | sudo tee -a $LOGPATH
+sudo apt-get install -y gcc | sudo tee -a $LOGPATH
+sudo apt-get install -y g++ | sudo tee -a $LOGPATH
+sudo apt install -y mysql-server | sudo tee -a $LOGPATH
+sudo apt-get install -y mongodb-org | sudo tee -a $LOGPATH
+sudo apt-get install -y vlc | sudo tee -a $LOGPATH
+sudo apt-get install -y gimp | sudo tee -a $LOGPATH
+sudo apt-get install -y inkscape | sudo tee -a $LOGPATH
+sudo apt install -y chromium-browser | sudo tee -a $LOGPATH
 
 # Stuff installed with Snap
-sudo snap install node --classic --channel=14
-sudo snap install --classic code
-sudo snap install postman
+sudo snap install node --classic --channel=14 | sudo tee -a $LOGPATH
+sudo snap install --classic code | sudo tee -a $LOGPATH
+sudo snap install postman | sudo tee -a $LOGPATH
 
 # Install essential vscode extensions
-code --install-extension mgmcdermott.vscode-language-babel
-code --install-extension ms-vscode.cpptools
-code --install-extension vscjava.vscode-java-debug
-code --install-extension vscjava.vscode-java-dependency
-code --install-extension vscjava.vscode-java-pack
-code --install-extension vscjava.vscode-java-test
-code --install-extension redhat.java
-code --install-extension vscjava.vscode-maven
-code --install-extension ms-python.python
-code --install-extension gruntfuggly.todo-tree
-code --install-extension visualstudioexptteam.vscodeintellicode
+code --install-extension mgmcdermott.vscode-language-babel | sudo tee -a $LOGPATH
+code --install-extension ms-vscode.cpptools | sudo tee -a $LOGPATH
+code --install-extension vscjava.vscode-java-debug | sudo tee -a $LOGPATH
+code --install-extension vscjava.vscode-java-dependency | sudo tee -a $LOGPATH
+code --install-extension vscjava.vscode-java-pack | sudo tee -a $LOGPATH
+code --install-extension vscjava.vscode-java-test | sudo tee -a $LOGPATH
+code --install-extension redhat.java | sudo tee -a $LOGPATH
+code --install-extension vscjava.vscode-maven | sudo tee -a $LOGPATH
+code --install-extension ms-python.python | sudo tee -a $LOGPATH
+code --install-extension gruntfuggly.todo-tree | sudo tee -a $LOGPATH
+code --install-extension visualstudioexptteam.vscodeintellicode | sudo tee -a $LOGPATH
 
-sudo apt upgrade
-sudo apt autoremove
+sudo apt upgrade -y | sudo tee -a $LOGPATH
+sudo apt autoremove -y | sudo tee -a $LOGPATH
